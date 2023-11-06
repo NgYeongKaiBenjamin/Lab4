bad6872927d92e70776d3b22f2b85e83d6fa3df6
Benjamin Ng <BENJAMINNG.22@ichat.sp.edu.sg>
2023-11-06 16:47:18 +0800

    PiLedTest.py

---
+++ src/PiLedTest.py
@@ -0,0 +1,30 @@
+import socket
+import time
+from time import sleep
+
+from hal import hal_led as led
+from hal import hal_input_switch as switch
+import version as ver
+
+def main():
+    led.init()
+    switch.init()
+    while True:
+        if switch.read_slide_switch()==1:
+            led.set_output(0, 1)
+            time.sleep(0.1)
+
+            led.set_output(0, 0)
+            time.sleep(0.1)
+        else:
+            led.set_output(0, 1)
+            time.sleep(0.05)
+
+            led.set_output(0, 0)
+            time.sleep(0.05)
+    
+    
+    
+    
+if __name__ == "__main__":
+    main()
